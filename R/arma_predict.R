ec2ab772e2413d14524ee161570bdda2719fd268
petercraigmile <pfc@stat.osu.edu>
2014-04-29 22:25:51 -0400

    Corrected documentation of arms.predict function

--- R/arma_predict.R
+++ R/arma_predict.R
@@ -4,9 +4,10 @@ arma.predict <- function (m, ar, ma, h=1) {
   ## ======================================================================
   ## arma.predict(m, ar, ma, h=1)
   ##
-  ## predict X_{m+h} based on X_{1}, ..., X_{m}, where {X_t} is an 
-  ## ARMA(p,q) process with AR coefficients 'ar' (of length p) and MA 
-  ## coefficients 'ma' (of length q).
+  ## Calculate the best linear prediction of X_{m+h} based on X_{1},
+  ## ..., X_{m}, where {X_t} is an ARMA(p,q) process with AR
+  ## coefficients 'ar' (of length p) and MA coefficients 'ma' (of
+  ## length q).
   ##
   ## pfc@stat.osu.edu
   ## ======================================================================
@@ -14,4 +15,5 @@ arma.predict <- function (m, ar, ma, h=1) {
   the.acf <- ARMAacf(ar=ar, ma=ma, lag.max=h+m-1)
   c(1, -solve(toeplitz(the.acf[1:m]), the.acf[(h+1):(m+h)]))
 }
+
 
